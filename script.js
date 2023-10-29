dd43103fb31388d2a8feeaf73fb29051d22a8491
Auridebson Lima <auridebson@vutus.com>
2023-10-29 20:37:05 -0300

    Update

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-let listaCompras = [
+const containerLista = document.querySelector(".container-lista")
+
+const produtos = [
     {nome:'Sabão em pó', preco:15, desconto:0.2},
     {nome:'Detergente', preco:2.5, desconto:0.1},
     {nome:'Oleo', preco:7.5, desconto:0.2},
@@ -8,11 +10,51 @@ let listaCompras = [
     {nome:'Cerveja 330ml Heineken', preco:7, desconto:0.18}
 ]
 
-function mostraNome(lista) {
-    lista += lista.vlrComDesconto
-    return lista.nome + " - " + parseInt(lista.preco * (1 - lista.desconto))
+function calculaPrecoDesconto(produtos) {
+    const produtosComDesconto = produtos.map(produto => {
+        const vlrComDesconto = produto.preco*(1-produto.desconto)
+        const vlrDesconto = produto.preco - (produto.preco*(1-produto.desconto))
+        return {
+            nome: produto.nome,
+            preco: produto.preco,
+            desconto: produto.desconto,
+            vlrDesconto: vlrDesconto,
+            vlrComDesconto: vlrComDesconto
+        }
+    })
+    return produtosComDesconto
+} //fim da função de adição de novo dado no array
+
+function calculaPrecoDesconto2(itens) {
+    const novosProdutos = itens.map((item) => {
+        const preco_descontado = item.preco-(item.preco*item.desconto)
+        return {
+            nome:item.nome,
+            preco:item.preco,
+            desconto:item.desconto,
+            preco_descontado:preco_descontado
+        }
+    })
+    return novosProdutos
 }
 
-const resultado = listaCompras.map(mostraNome)
+function calculaPrecoDesconto3(itens) {
+    const novosProdutos = itens.map(item => {
+        const preco_descontado = parseInt(item.preco-(item.preco*item.desconto))
+        return {
+            nome: item.nome,
+            preco: item.preco,
+            preco_descontado: preco_descontado
+        }
+    })
+    return novosProdutos
+}
 
-console.log(resultado)
+
+let novoArray = calculaPrecoDesconto3(produtos)
+
+console.log(calculaPrecoDesconto3(novoArray))
+for (i in novoArray) {
+    console.log(novoArray[i])
+}
+
